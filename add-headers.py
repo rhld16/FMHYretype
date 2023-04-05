c86002a2c7e61dd94f6fc4cc1c40a846b85de51e
rhld16 <24809571+rhld16@users.noreply.github.com>
2023-04-05 14:03:43 +0000

    final header data

--- add-headers.py
+++ add-headers.py
@@ -1,7 +1,27 @@
 import os
 
 headersData = {
-    "AdblockVPNGuide.md": [":name_badge:", "140", "# Adblocking / Privacy", "Adblocking, Privacy, VPN's, Proxies, Antivirus"]
+    "AdblockVPNGuide.md":       [":name_badge:",        "140", "# Adblocking / Privacy", "Adblocking, Privacy, VPN's, Proxies, Antivirus"],
+    "AI.md":                    [":robot_face:",        "130", "# Artificial Intelligence", "Chat Bots, Text Generators, Image Generators, ChatGPT Tools"],
+    "Android-iOSGuide.md":      [":iphone:",            "45", "# Android / iOS", "Apps, Jailbreaking, Android Emulators"],
+    "AudioPiracyGuide.md":      [":musical_note:",      "110", "# Music / Podcasts / Radio", "Stream Audio, Download Audio, Torrent Audio"],
+    "Beginners-Guide.md":       [":pirate_flag:",       "150", "# Beginners Guide to Piracy", ""],
+    "DEVTools.md":              [":male-technologist:", "50", "# Developer Tools", ""],
+    "DownloadPiracyGuide.md":   [":floppy_disk:",       "80", "# Downloading", "Download Sites, Software Sites, Open Directories"],
+    "EDUPiracyGuide.md":        [":brain:",             "60", "# Educational", "Courses, Documentaries, Learning Resources"],
+    "GamingPiracyGuide.md":     [":video_game:",        "100", "# Gaming / Emulation", "Download Games, ROMs, Gaming Tools"],
+    "LinuxGuide.md":            [":penguin:",           "40", "# Linux / MacOS", "Apps, Software Sites, Gaming"],
+    "AdblockVPNGuide.md":       [":name_badge:",        "140", "# Adblocking / Privacy", "Adblocking, Privacy, VPN's, Proxies, Antivirus"],
+    "MISCGuide.md":             [":open_file_folder:",  "30", "# Miscellaneous", "Extensions, Indexes, News, Health, Food, Fun"],
+    "NSFWPiracy.md":            [":underage:",          "25", "# NSFW", ""],
+    "Non-English.md":           [":earth_asia:",        "35", "# Non-English", "International Piracy Sites"],
+    "ReadingPiracyGuide.md":    [":green_book:",        "90", "# Books / Comics / Manga", "Books, Comics, Magazines, Newspapers"],
+    "STORAGE.md":               [":card_file_box:",     "1", "# NSFW", ""],
+    "TOOLSGuide.md":            [":wrench:",            "58", "# Tools", "General Tools, Internet Tools, System Tools"],
+    "TorrentPiracyGuide.md":    [":cyclone:",           "70", "# Torrenting", ""],
+    "VideoPiracyGuide.md":      [":tv:",                "120", "# Movies / TV / Anime", "Stream Videos, Download Videos, Torrent Videos"],
+    "base64.md":                [":key:",               "20", "", ""],
+    "img-tools.md":             [":camera:",            "55", "# Image Tools", ""]
 }
 
 def getHeaderForPage(pageFilename):
